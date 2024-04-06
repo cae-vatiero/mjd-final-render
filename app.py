e7cbbc7d9ceeedce229434684c5476e201315a74
cae-vatiero <cae.vatiero@gmail.com>
2024-04-06 10:43:21 -0300

    alteração inicial

--- app.py
+++ app.py
@@ -2,6 +2,18 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/home")
 def index():
   return "Esse é um teste do site"
+
+@app.route("/sobremim")
+def sobremim():
+  return "teste sobre mim lala"
+
+@app.route ("/portfolio")
+def porfolio():
+  return "meu porfolio"
+
+@app.route("/contato")
+def contato():
+  return "meus contatos"
