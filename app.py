0e674e51b4b10465590b8e22c345b8617262969c
cae-vatiero <cae.vatiero@gmail.com>
2024-04-06 16:01:55 -0300

    commit dnvdnv

--- app.py
+++ app.py
@@ -6,10 +6,6 @@ app = Flask(__name__)
 def index():
   return render_template("home.html")
 
-@app.route("/home")
-def index():
-  return render_template("home.html")
-
 @app.route("/sobremim")
 def sobremim():
   return render_template("sobremim.html")
