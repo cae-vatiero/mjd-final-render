34c99938f1048ea19b30539ce8ebe6170210eb4b
cae-vatiero <cae.vatiero@gmail.com>
2024-04-06 12:28:16 -0300

    nova alteracao

--- app.py
+++ app.py
@@ -2,18 +2,18 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/home")
+@app.route("/")
 def index():
   return render_template("home.html")
 
 @app.route("/sobremim")
 def sobremim():
-  return "sobremim.html"
+  return render_template("sobremim.html")
 
 @app.route ("/portfolio")
 def porfolio():
-  return "porfolio.html"
+  return render_template("porfolio.html")
 
 @app.route("/contato")
 def contato():
-  return "contato.html"
+  return render_template("contato.html")
